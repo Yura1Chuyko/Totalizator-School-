c884cf3f23e6358a6cb491c948840164e266da6f
Yura <ura5509@gmail.com>
2022-10-29 20:13:22 +0300

    almostLast

--- force-app/main/default/aura/currentWeekGames/currentWeekGamesController.js
+++ force-app/main/default/aura/currentWeekGames/currentWeekGamesController.js
@@ -4,7 +4,7 @@
             {label: 'Name', fieldName: 'Name', type: 'Text'},
             {label: 'Total Money Bet', fieldName: 'Total_Money_Bet__c', type: 'Number'},
             {label: 'Total Income', fieldName: 'Total_Income__c', type: 'Number'},
-            {label: 'Start Date', fieldName: 'Start_Date__c', type: 'Date/Time'}
+            {label: 'Start Date', fieldName: 'Start_Date__c', type: 'DateTime'}
         ]);
         helper.getData(cmp);
     }
